fa897a6ddceb628d77e0342b90e10d4580e829d6
Brandon Coffey <bmc180001@utdallas.edu>
2022-05-03 22:21:09 -0500

    Basic Main Menu / Settings Menu Navigation

---
+++ Assets/_Game/Scripts/Input/MainMenuNavigation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuNavigation : MonoBehaviour
+{
+    [SerializeField] private bool _debug = false;
+    [SerializeField] private GameObject _startGameButton = null;
+    [SerializeField] private GameObject _options1FirstButton = null;
+    [SerializeField] private GameObject _options2FirstButton = null;
+
+    private GameObject _current;
+
+    private void Update() {
+        if (_debug) {
+            var current = EventSystem.current.currentSelectedGameObject;
+            if (_current != current) {
+                _current = current;
+                Debug.Log("Selected: " + current, current);
+            }
+        }
+    }
+
+    public void NavigateMainMenu() {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(_startGameButton);
+    }
+    public void NavigateOptions1()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(_options1FirstButton);
+    }
+    public void NavigateOptions2()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(_options2FirstButton);
+    }
+}
